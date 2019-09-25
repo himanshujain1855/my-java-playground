99b991ccd89b9713a12da764bedd4fee76bf64d4
himanshujain1855 <49681798+himanshujain1855@users.noreply.github.com>
2019-09-25 11:49:30 +0530

    Added Seperate Function to check leap year!
    This Code Helps to write and understand, how to write a code to check whether given year is leap year or not!

--- basic-programs/LeapCalculator.java
+++ basic-programs/LeapCalculator.java
@@ -2,26 +2,33 @@ package programs;
 import java.util.Scanner;
 
 public class LeapCalculator {
+
+    static boolean checkIsLeapYear(int year){
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int year;
         year=sc.nextInt();
-
-        if(year%4==0){
-            if(year%100==0){
-                if(year%400==0){
-                    System.out.println("Yes");
-                }
-                else{
-                    System.out.println("No");
-                }
-            }
-            else{
-                System.out.println("Yes");
-            }
+        if(checkIsLeapYear(year)){
+            System.out.println("yes");
         }
-        else{
-            System.out.println("No");
-        }
+        else System.out.println("No");
     }
 }
